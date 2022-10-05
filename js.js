f6e88066cfd3b67b249e90e5dc06b4386e1c9cc7
Coltensiemer <colten50@hotmail.com>
2022-10-05 15:54:40 -0400

    update
    add additional prompts and media

--- js.js
+++ js.js
@@ -1,30 +1,55 @@
-const name = document.getElementById('name')
-const password = document.getElementById('password') 
-const login_Btn = document.getElementById('submit')
-const form = document.getElementById('form')
-const errorElement = document.getElementById('error')
-const submit = document.getElementById('submit'); 
+let id = (id) => document.getElementById(id);
+let classes = (classes) => document.getElementsByClassName(classes);
+
+let username = id('name'); 
+let email = id('email'); 
+let password = id('password'); 
+let form = id('form'); 
+let submit = id('submit');
+let signUp= id('signup-btn');
+
+let error = classes('error'); 
 
 
-function validation() { 
-    const name = document.getElementById('name')
-    const password = document.getElementById('password') 
-    const form = document.getElementById('form')
-    const errorElement = document.getElementById('error')
-   
 
-   
-    const validate = (e) => { 
-        e.preventDefault(); 
 
-    if (name.value === " " || password.value === " "){
-        prompt("Enter your name"); 
-        name.focus(); 
-        return false; 
+
+// Event Listener for form ==================
+form.addEventListener('submit', (e) => { 
+    e.preventDefault();
+engine(username,0,"Username cannot be blank"); 
+engine(email, 1, "Email can not be blank"); 
+engine(password, 2, "Password cannot be blank"); 
+
+});
+
+
+
+
+
+// This ENGINE DOES ALL THE WORK =========
+let engine = (id, serial, message) => { 
+    if (id.value.trim() === "") { 
+        error[serial].innerHTML = message; 
+        id.style.border = "2px red solid";
     }
+    else { error[serial].innerHTML = "";
+        id.style.border ="2px green solid"; 
+    }
+} 
 
-    } 
+
+
+
+
+
+
+
+
+
+// SIGN UP LINK----------------------------------
+signUp.addEventListener('click', function() { 
+alert('This link does not work') 
+signUp.style.color = "red"; 
 }
-
-    submit.addEventListener('click', validate);
-
+) 
